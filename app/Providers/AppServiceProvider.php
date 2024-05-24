78e49a0bb93a9057a4462893346f33136ecd3dbf
Janzen Faidiban <janzenfaidiban@gmail.com>
2024-05-24 11:54:01 +0700

    feat: setup site information di app service provider untuk favicon, meta images, metaTitle, metaDescription, dll

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::unguard();        
+
+        View::share('siteTitle', 'Data Lulusan Peserta Didik Provinsi Papua');
+        View::share('siteDescription', 'Sistem Informasi Data Lulusan Peserta Didik Provinsi Papua');
+        View::share('siteFavicon', 'favicon.png');
+        View::share('siteLogo', 'logo_provinsi_papua.png');
+        View::share('siteDomain', 'https://datalulusanpapua.info');
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,17 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{!! ('Data Lulusan Siswa Provinsi Papua') !!}</title>
+    <title>{!! $siteTitle ?? '' !!}</title>
     <meta property="og:locale" content="id_ID" />
     <meta property="og:type" content="website" />
-    <meta property="og:title" content="Data Lulusan Siswa Provinsi Papua" />
-    <meta property="og:description" content="Data Lulusan Siswa Provinsi Papua" />
-    <meta property="og:url" content="https://datalulusanpapua.info" />
-    <meta property="og:site_name" content="Data Lulusan Siswa Provinsi Papua" />
-    <meta property="og:image" content="https://datalulusanpapua.info/favicon.png" />
+    <meta property="og:title" content="{!! $siteTitle ?? '' !!}" />
+    <meta property="og:description" content="{!! $siteDescription ?? '' !!}" />
+    <meta property="og:url" content="{!! $siteDomain ?? '' !!}" />
+    <meta property="og:site_name" content="{!! $siteTitle ?? '' !!}" />
+    <meta property="og:image" content="{!! $siteLogo ?? 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Coat_of_arms_of_Papua_2.svg/1200px-Coat_of_arms_of_Papua_2.svg.png' !!}" />
 
     <!-- Favicon  -->
-    <link rel="icon" href="{{ asset('favicon.png') }}" />
+    <link rel="icon" href="{!! $siteLogo ?? 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Coat_of_arms_of_Papua_2.svg/1200px-Coat_of_arms_of_Papua_2.svg.png' !!}" />
 
     <!-- CSS Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -25,16 +25,16 @@
 </head>
 <body>
 
-    <header class="p-3 p-xl-5">
-        <div class="row pt-0 pt-xl-5">
-            <div class="col-xl-6 mx-auto">
+    <header class="p-3 p-xl-5 bg-primary text-white shadow">
+        <div class="row pt-0 ">
+            <div class="col-xl-8">
                 <div class="text-center text-xl-start d-sm-inline-block d-xl-flex gap-4">
                     <div>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Logo_of_Ministry_of_Education_and_Culture_of_Republic_of_Indonesia.svg/220px-Logo_of_Ministry_of_Education_and_Culture_of_Republic_of_Indonesia.svg.png" alt="Tut Wuri Handayani" width="100px">
+                        <img src="{!! $siteLogo ?? 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Coat_of_arms_of_Papua_2.svg/1200px-Coat_of_arms_of_Papua_2.svg.png' !!}" alt="Tut Wuri Handayani" width="80px">
                     </div>
                     <div>
-                        <h1 class="fw-bold">Data Kelulusan Siswa</h1>
-                        <p>Cari berdasarkan Jenjang, NPSN, Nama Sekolah, Nama Peserta, dan Tahun Lulus.</p>
+                        <h1 class="fw-bold">{!! $siteTitle ?? '' !!}</h1>
+                        <p class="fs-4">{!! $siteDescription ?? '' !!}</p>
                     </div>
                 </div>
             </div>
