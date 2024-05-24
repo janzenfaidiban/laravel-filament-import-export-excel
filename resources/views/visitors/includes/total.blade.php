0ff82a1ed22819f7afe45f466a826f633ce71275
Janzen Faidiban <janzenfaidiban@gmail.com>
2024-05-25 00:19:20 +0900

    feat: tambah line height, dan ubah judul dan button total dark

--- resources/views/visitors/includes/total.blade.php
+++ resources/views/visitors/includes/total.blade.php
@@ -3,43 +3,43 @@
         
         <div class="row">
             <div class="col-xl-3 col-md-6 mb-3">
-                <a href="{{ url('hasilPencarian?jenjang=SD') }}" class="btn btn-outline-primary rounded-0 w-100">
+                <a href="{{ url('hasilPencarian?jenjang=SD') }}" class="btn btn-outline-dark rounded-0 w-100">
                     SD: <span class="fw-bold">{{ $totalSD ?? 'Total SD' }} Siswa</span>
                 </a>
             </div>
 
             <div class="col-xl-3 col-md-6 mb-3">
-                <a href="{{ url('hasilPencarian?jenjang=SMP') }}" class="btn btn-outline-primary rounded-0 w-100">
+                <a href="{{ url('hasilPencarian?jenjang=SMP') }}" class="btn btn-outline-dark rounded-0 w-100">
                     SMP: <span class="fw-bold">{{ $totalSMP ?? 'Total SMP' }} Siswa</span>
                 </a>
             </div>
             
             <div class="col-xl-3 col-md-6 mb-3">
-                <a href="{{ url('hasilPencarian?jenjang=SMA') }}" class="btn btn-outline-primary rounded-0 w-100">
+                <a href="{{ url('hasilPencarian?jenjang=SMA') }}" class="btn btn-outline-dark rounded-0 w-100">
                     SMA: <span class="fw-bold">{{ $totalSMA ?? 'Total SMA' }} Siswa</span>
                 </a>
             </div>
             
             <div class="col-xl-3 col-md-6 mb-3">
-                <a href="{{ url('hasilPencarian?jenjang=SMK') }}" class="btn btn-outline-primary rounded-0 w-100">
+                <a href="{{ url('hasilPencarian?jenjang=SMK') }}" class="btn btn-outline-dark rounded-0 w-100">
                     SMK: <span class="fw-bold">{{ $totalSMK ?? 'Total SMK' }} Siswa</span>
                 </a>
             </div>
             
             <div class="col-xl-3 col-md-6 mb-3">
-                <a href="{{ url('hasilPencarian?jenjang=Paket-A') }}" class="btn btn-outline-primary rounded-0 w-100">
+                <a href="{{ url('hasilPencarian?jenjang=Paket-A') }}" class="btn btn-outline-dark rounded-0 w-100">
                     Paket-A: <span class="fw-bold">{{ $totalPaketA ?? 'Total Paket-A' }} Siswa</span>
                 </a>
             </div>
             
             <div class="col-xl-3 col-md-6 mb-3">
-                <a href="{{ url('hasilPencarian?jenjang=Paket-B') }}" class="btn btn-outline-primary rounded-0 w-100">
+                <a href="{{ url('hasilPencarian?jenjang=Paket-B') }}" class="btn btn-outline-dark rounded-0 w-100">
                     Paket-B: <span class="fw-bold">{{ $totalPaketB ?? 'Total Paket-B' }} Siswa</span>
                 </a>
             </div>
             
             <div class="col-xl-3 col-md-6 mb-3">
-                <a href="{{ url('hasilPencarian?jenjang=Paket-C') }}" class="btn btn-outline-primary rounded-0 w-100">
+                <a href="{{ url('hasilPencarian?jenjang=Paket-C') }}" class="btn btn-outline-dark rounded-0 w-100">
                     Paket-C: <span class="fw-bold">{{ $totalPaketC ?? 'Total Paket-C' }} Siswa</span>
                 </a>
             </div>
