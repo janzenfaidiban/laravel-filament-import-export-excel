3b1bc622a019ef9645fa59fba94941d8974ea4a3
Janzen Faidiban <janzenfaidiban@gmail.com>
2024-05-24 11:34:22 +0700

    feat: tambah meta tags dan ganti favicon logo provinsi papua

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Data Kelulusan Siswa</title>
+    <title>{!! ('Data Lulusan Siswa Provinsi Papua') !!}</title>
+    <meta property="og:locale" content="id_ID" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Data Lulusan Siswa Provinsi Papua" />
+    <meta property="og:description" content="Data Lulusan Siswa Provinsi Papua" />
+    <meta property="og:url" content="https://datalulusanpapua.info" />
+    <meta property="og:site_name" content="Data Lulusan Siswa Provinsi Papua" />
+    <meta property="og:image" content="https://datalulusanpapua.info/favicon.png" />
+
+    <!-- Favicon  -->
+    <link rel="icon" href="{{ asset('favicon.png') }}" />
 
     <!-- CSS Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
