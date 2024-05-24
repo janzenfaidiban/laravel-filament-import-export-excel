272321b582aeec31ec51be38c81e77047269d29f
Janzen Faidiban <janzenfaidiban@gmail.com>
2024-05-25 00:09:07 +0900

    feat: tambah background

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 </head>
-<body>
+<body style="background-image: url({{ asset('bg.jpg') }})">
 
     <header class="p-3 p-xl-5 bg-primary text-white shadow">
         <div class="row pt-0 ">
