810e99209cce01ed19a0ef2090542faf4243d743
Janzen Faidiban <janzenfaidiban@gmail.com>
2024-05-25 00:04:46 +0900

    feat: ubah jenjang pake select/dropdown

--- resources/views/visitors/includes/form.blade.php
+++ resources/views/visitors/includes/form.blade.php
@@ -5,10 +5,26 @@
         <input type="text" class="form-control" id="kab_kota" name="kab_kota" value="@if(request()->get('kab_kota')){{ request()->get('kab_kota') }}@endif">
     </div> <!-- input item end -->
 
-    <div class="mb-3">
+    {{-- <div class="mb-3">
         <label for="jenjang" class="form-label">Jenjang</label>
         <input type="text" class="form-control" id="jenjang" name="jenjang" value="@if(request()->get('jenjang')){{ request()->get('jenjang') }}@endif">
+    </div> <!-- input item end --> --}}
+
+    <div class="mb-3">
+        <label for="jenjang" class="form-label">Jenjang</label>
+        <select class="form-select form-select" aria-label="Small select example" id="jenjang" name="jenjang">
+            <option selected></option>
+            <option value="SD" @if(isset($_GET['jenjang']) && $_GET['jenjang'] == 'SD') selected @endif>SD</option>
+            <option value="SMP" @if(isset($_GET['jenjang']) && $_GET['jenjang'] == 'SMP') selected @endif>SMP</option>
+            <option value="SMA" @if(isset($_GET['jenjang']) && $_GET['jenjang'] == 'SMA') selected @endif>SMA</option>
+            <option value="SMK" @if(isset($_GET['jenjang']) && $_GET['jenjang'] == 'SMK') selected @endif>SMK</option>
+            <option value="Paket-A" @if(isset($_GET['jenjang']) && $_GET['jenjang'] == 'Paket-A') selected @endif>Paket-A</option>
+            <option value="Paket-B" @if(isset($_GET['jenjang']) && $_GET['jenjang'] == 'Paket-B') selected @endif>Paket-B</option>
+            <option value="Paket-C" @if(isset($_GET['jenjang']) && $_GET['jenjang'] == 'Paket-C') selected @endif>Paket-C</option>
+          </select>
     </div> <!-- input item end -->
+
+
 
     <div class="mb-3">
         <label for="npsn" class="form-label">NPSN</label>
--- resources/views/visitors/includes/total.blade.php
+++ resources/views/visitors/includes/total.blade.php
@@ -2,32 +2,46 @@
     <div class="col-xl-8 mx-xl-auto">
         
         <div class="row">
-            <div class="col-xl-3 col-md-6 border rounded-lg shadow p-3 text-secondary">
-                Total SD: <span class="fw-bold text-dark">{{ $totalSD ?? 'Total SD' }} Siswa</span>
+            <div class="col-xl-3 col-md-6 mb-3">
+                <a href="{{ url('hasilPencarian?jenjang=SD') }}" class="btn btn-outline-primary w-100">
+                    SD: <span class="fw-bold">{{ $totalSD ?? 'Total SD' }} Siswa</span>
+                </a>
+            </div>
+
+            <div class="col-xl-3 col-md-6 mb-3">
+                <a href="{{ url('hasilPencarian?jenjang=SMP') }}" class="btn btn-outline-primary w-100">
+                    SMP: <span class="fw-bold">{{ $totalSMP ?? 'Total SMP' }} Siswa</span>
+                </a>
             </div>
             
-            <div class="col-xl-3 col-md-6 border rounded-lg shadow p-3 text-secondary">
-                Total SMP: <span class="fw-bold text-dark">{{ $totalSMP ?? 'Total SMP' }} Siswa</span>
+            <div class="col-xl-3 col-md-6 mb-3">
+                <a href="{{ url('hasilPencarian?jenjang=SMA') }}" class="btn btn-outline-primary w-100">
+                    SMA: <span class="fw-bold">{{ $totalSMA ?? 'Total SMA' }} Siswa</span>
+                </a>
             </div>
             
-            <div class="col-xl-3 col-md-6 border rounded-lg shadow p-3 text-secondary">
-                Total SMA: <span class="fw-bold text-dark">{{ $totalSMA ?? 'Total SMA' }} Siswa</span>
+            <div class="col-xl-3 col-md-6 mb-3">
+                <a href="{{ url('hasilPencarian?jenjang=SMK') }}" class="btn btn-outline-primary w-100">
+                    SMK: <span class="fw-bold">{{ $totalSMK ?? 'Total SMK' }} Siswa</span>
+                </a>
             </div>
             
-            <div class="col-xl-3 col-md-6 border rounded-lg shadow p-3 text-secondary">
-                Total SMK: <span class="fw-bold text-dark">{{ $totalSMK ?? 'Total SMK' }} Siswa</span>
+            <div class="col-xl-3 col-md-6 mb-3">
+                <a href="{{ url('hasilPencarian?jenjang=Paket-A') }}" class="btn btn-outline-primary w-100">
+                    Paket-A: <span class="fw-bold">{{ $totalPaketA ?? 'Total Paket-A' }} Siswa</span>
+                </a>
             </div>
             
-            <div class="col-xl-3 col-md-6 border rounded-lg shadow p-3 text-secondary">
-                Total Paket-A: <span class="fw-bold text-dark">{{ $totalPaketA ?? 'Total Paket-A' }} Siswa</span>
+            <div class="col-xl-3 col-md-6 mb-3">
+                <a href="{{ url('hasilPencarian?jenjang=Paket-B') }}" class="btn btn-outline-primary w-100">
+                    Paket-B: <span class="fw-bold">{{ $totalPaketB ?? 'Total Paket-B' }} Siswa</span>
+                </a>
             </div>
             
-            <div class="col-xl-3 col-md-6 border rounded-lg shadow p-3 text-secondary">
-                Total Paket-B: <span class="fw-bold text-dark">{{ $totalPaketB ?? 'Total Paket-B' }} Siswa</span>
-            </div>
-            
-            <div class="col-xl-3 col-md-6 border rounded-lg shadow p-3 text-secondary">
-                Total Paket-C: <span class="fw-bold text-dark">{{ $totalPaketC ?? 'Total Paket-C' }} Siswa</span>
+            <div class="col-xl-3 col-md-6 mb-3">
+                <a href="{{ url('hasilPencarian?jenjang=Paket-C') }}" class="btn btn-outline-primary w-100">
+                    Paket-C: <span class="fw-bold">{{ $totalPaketC ?? 'Total Paket-C' }} Siswa</span>
+                </a>
             </div>
 
         </div>
